cb92826f13089ee412d350f978793665b16e204b
R Aishwarya Prabhu <aishwaryaprabhur7@gmail.com>
2025-04-19 13:32:17 +0530

    [feat]: Frontend Setup

---
+++ front/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function App(){
+  return(
+    <div>
+      <h1 className="text-red-500">I am home page</h1>
+      <Button>Hello next</Button>
+    </div>
+  );
+}
